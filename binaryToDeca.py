9ed8fb8960c4cb79e8bc4f678798b97c0ac63582
foxpower01 <rin.studys@gmail.com>
2022-02-03 09:58:34 -0800

    decimal and signs working

--- binaryToDeca.py
+++ binaryToDeca.py
@@ -1,16 +1,30 @@
 places = []
+deciPlaces = []
 output = 0
+decimal = 0
+isDecimal = False
 isNegative = False
-input = input("Binary number, first bit determines sign:\n")
+input = input("Binary number, first bit determines sign, use a '.' to indicate decimals:\n")
 input = list(input)
 for item in input[::-1]:
-    places.append(item)
-if places[-1] == 0:
+    if item == ".":
+        isDecimal = True
+    else:
+        places.append(item)
+for d in range(len(input)):
+    if input.index(".") < d:
+        deciPlaces.append(input[d])
+        places.remove(input[d])
+if places[-1] == "0":
     isNegative = True
 places.pop()
 print(list(places))
 for i in range(len(list(places))):
     output += 2 ** i * int(list(places)[i])
+for j in range(len(deciPlaces)):
+    decimal += 2 ** -(j + 1) * int(deciPlaces[j])
 if isNegative:
     output = -1 * output
-print(output)
+    decimal = -1 * decimal
+print(output + decimal)
+print(deciPlaces)
