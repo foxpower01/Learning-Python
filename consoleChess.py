fbe873de4f26016702af9f981fe8a2474bb3b5cb
foxpower01 <rin.studys@gmail.com>
2022-02-28 22:55:55 -0800

    workin on it

--- consoleChess.py
+++ consoleChess.py
@@ -1,11 +1,23 @@
 # [["pawn", 1]["rook", 2]["knight", 3]["bishop", 4]["queen", 5]["king", 6]["empty", 0]]
 
+whiteTaken = []
+blackTaken = []
+
 class Piece(object):
 
     def __init__(self, pieceType, piecePosition, pieceColor):
         self.type = pieceType
         self.position = piecePosition
         self.color = pieceColor
+
+    def getColor(self):
+        return(self.color)
+
+    def getPos(self):
+        return(self.position)
+
+    def getType(self):
+        return(self.type)
 
 class Pawn(Piece):
 
@@ -39,6 +51,23 @@ class King(Piece):
 
 class Board(object):
 
-    def __init__(self, boardState = [[2, 3, 4, 5, 6, 4, 3, 2],[1, 1, 1, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 1, 1, 1],[2, 3, 4, 5, 6, 4, 3, 2]]):
-        pass
+    def __init__(self, boardState = [[2, 3, 4, 5, 6, 4, 3, 2],[1, 1, 1, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 1, 1, 1],[2, 3, 4, 5, 6, 4, 3, 2]], boardColor = "white"):
+        self.state = boardState
+        self.color = boardColor
 
+    def getBoaradState(self):
+        return(self.state)
+
+    def setBoardState(self, piece, posOld, posNew):
+        #positions come as list [row, column]
+        global whiteTaken
+        global blackTaken
+        self.state[posOld[1]][posOld[2]] = 0
+        if self.state[posNew[1]][posNew[2]]:
+            if piece.getcolor() == "white":
+                whiteTaken.append(self.state[posNew[1]][posNew[2]])
+            else:
+                blackTaken.append(self.state[posNew[1]][posNew[2]])
+            self.state[posNew[1]][posNew[2]] = piece.getType()
+        else:
+            self.state[posNew[1]][posNew[2]] = piece.getType()
