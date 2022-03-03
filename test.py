a2d41c28c47d3e30fdb1efe6541589c5b57b8fa5
fox_power <rin.studys@gmail.com>
2022-03-03 10:05:06 -0800

    in prog

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-def primeFactors(number):
-    factor = 2
-    primeFactors = []
-    while factor * factor <= number:
-        if number % factor:
-            factor += 1
-        else:
-            number //= factor
-            primeFactors.append(factor)
-    if number > 1:
-        primeFactors.append(number)
-    return(primeFactors)
-
-print(primeFactors(256))
+for i in range(2, 10):
+    print(i)
