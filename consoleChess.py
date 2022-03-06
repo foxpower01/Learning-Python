5764514695bb3df4680a358125b615d089077b45
foxpower01 <rin.studys@gmail.com>
2022-03-05 23:33:15 -0800

    board stores color now,  also king progress

--- consoleChess.py
+++ consoleChess.py
@@ -1,7 +1,35 @@
-# [["pawn", 1]["rook", 2]["knight", 3]["bishop", 4]["queen", 5]["king", 6]["empty", 0]]
+# [["pawn", 1]["rook", 2]["knight", 3]["bishop", 4]["queen", 5]["king", 6]["empty", 0]], COLORS: [[0, black], [1, white]]
 
 whiteTaken = []
 blackTaken = []
+
+class Board(object):
+
+    def __init__(self, boardState = [[[2,0], [3,0], [4,0], [5,0], [6,0], [4,0], [3,0], [2,0]],[[1,0], [1,0], [1,0], [1,0], [1,0], [1,0], [1,0], [1,0]],[[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]],[[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]],[[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]],[[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]],[[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]],[[2,1], [3,1], [4,1], [5,1], [6,1], [4,1], [3,1], [2,1]]], boardColor = "white"):
+        self.state = boardState
+        self.color = boardColor
+
+    def getBoaradState(self):
+        return(self.state)
+
+    def getPosition(self, position):
+        return(self.state[position[0]][position[1]])
+
+    def setBoardState(self, piece, posOld, posNew):
+        #positions come as list [row, column]
+        global whiteTaken
+        global blackTaken
+        self.state[posOld[0]][posOld[1]] = [0,0]
+        if self.state[posNew[0]][posNew[1]][0]:
+            if piece.getcolor() == 1:
+                whiteTaken.append(self.state[posNew[0]][posNew[1]])
+            else:
+                blackTaken.append(self.state[posNew[0]][posNew[1]])
+            self.state[posNew[0]][posNew[1]] = piece.getType()
+        else:
+            self.state[posNew[0]][posNew[1]] = piece.getType()
+
+board = Board()
 
 class Piece(object):
 
@@ -18,6 +46,11 @@ class Piece(object):
 
     def getType(self):
         return(self.type)
+
+    def canTake(self, posNew):
+        if self.color == board.getPosition(posNew)[1]:
+            return(False)
+        return(True)
 
 class Pawn(Piece):
 
@@ -48,26 +81,17 @@ class King(Piece):
 
     def __init__(self, pieceType, piecePosition, pieceColor):
         super().__init__(pieceType, piecePosition, pieceColor)
+    
+    def isCheck(self, position):
+        return(False)
 
-class Board(object):
+    def canMove(self, posNew):
+        if self.position[0] == posNew[0]:
+            if abs(self.position[1] - posNew[1]) == 1:
+                if self.canTake:
+                    return(True)
+        if self.position[1] == posNew[1]:
+            if abs(self.position[0] - posNew[0]) == 1:
+                if self.canTake:
+                    return(True)
 
-    def __init__(self, boardState = [[2, 3, 4, 5, 6, 4, 3, 2],[1, 1, 1, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 1, 1, 1],[2, 3, 4, 5, 6, 4, 3, 2]], boardColor = "white"):
-        self.state = boardState
-        self.color = boardColor
-
-    def getBoaradState(self):
-        return(self.state)
-
-    def setBoardState(self, piece, posOld, posNew):
-        #positions come as list [row, column]
-        global whiteTaken
-        global blackTaken
-        self.state[posOld[1]][posOld[2]] = 0
-        if self.state[posNew[1]][posNew[2]]:
-            if piece.getcolor() == "white":
-                whiteTaken.append(self.state[posNew[1]][posNew[2]])
-            else:
-                blackTaken.append(self.state[posNew[1]][posNew[2]])
-            self.state[posNew[1]][posNew[2]] = piece.getType()
-        else:
-            self.state[posNew[1]][posNew[2]] = piece.getType()
