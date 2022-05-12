e4f95e6f8a273f3d805caf481ffc36217a033339
fox_power <rin.studys@gmail.com>
2022-05-12 10:04:23 -0700

    coviding

--- COVIDdata.py
+++ COVIDdata.py
@@ -1,5 +1,16 @@
 import matplotlib.pyplot as plt
+import json
+import urllib.request
+
+url = "https://api.covidtracking.com/v1/us/daily.json"
+
+def readdata():
+  uh = urllib.request.urlopen(url)
+  str_data = uh.read()
+  return json.loads(str_data)
+
+print(readdata()[1])
+
 plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-plt.ylabel('some numbers')
-plt.xlabel("jacks")
+
 plt.show()
